c42fe811e7ef599919f4de559b9ac18d5ff60030
vikramaulakh <46392235+vikramaulakh@users.noreply.github.com>
2019-03-13 20:00:29 -0400

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,33 @@
+package co.vik.selenium.demo;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class Demo {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver","C:\\New folder (2)\\zip_jar_exe\\exe files\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://www.gmail.com/");
+		driver.manage().window().maximize();
+		driver.findElement(By.id("identifierId")).sendKeys("vikramdpaulakh@gmail.com");
+		driver.findElement(By.className("CwaK9")).click();
+		String at = driver.getTitle();
+		String et = "gmail";
+		driver.close();
+		if(at.equalsIgnoreCase(et))
+		{
+			System.out.println("Test successful");
+		}
+		else
+		{
+			System.out.println("Test Failure");
+			
+		}
+	}
+}
+		
+		
+		
